2a7f9fb226c1d39d50dab885a78b8b1569de2b9c
lampitosgames <dantimko1@gmail.com>
2019-04-05 14:14:36 -0400

    Full ESLint and SCSS support

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      Time to code!
+      </div>
+    );
+  }
+}
+
+export default App;
