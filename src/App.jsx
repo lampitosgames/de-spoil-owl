7678c5c742c76c37c73c5f672375cb7eecd1040f
lampitosgames <dantimko1@gmail.com>
2019-04-05 14:36:21 -0400

    Added circleci config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      Time to code!
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">Time to code!</div>
+);
 
 export default App;
